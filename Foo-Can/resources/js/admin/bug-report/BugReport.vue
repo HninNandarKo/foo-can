983179f2ed3655577300e3fcfb16f6bfc449cefc
Pyae Phyo Aung <pyaephyoaungtcl.98@gmail.com>
2020-06-18 14:38:42 +0630

    first commit

---
+++ Foo-Can/resources/js/admin/bug-report/BugReport.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        BUG Report Page
+    </div>
+</template>
+<script></script>
+export default {
+    mounted(){
+        console.log("this is bug report page");
+        const self = this;
+
+            self.$store.commit('setBreadcrumbs',[
+                {label:'Bug Report',name:''}
+            ]);
+    }
+}
+</script>
